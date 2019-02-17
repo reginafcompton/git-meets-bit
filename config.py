31b064beb19d66c838249cb60380407bc5473cab
reginafcompton <reginafcompton@gmail.com>
2019-02-17 10:27:52 -0500

    Initial flask app for selective Github profile

---
+++ config.py
@@ -0,0 +1,5 @@
+from secrets import ACCESS_TOKEN
+
+HEADERS_GITHUB = {'Accept': 'application/vnd.github.mercy-preview+json'}
+BASE_URL_GITHUB = 'https://api.github.com/users/'
+GITHUB_ACCESS_TOKEN_URL = '?access_token=' + ACCESS_TOKEN
